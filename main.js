12e61736d5633b80c0c0abfcc40366b28a4c34ea
Elmi Rabelo <rabeloelmi@gmail.com>
2019-05-20 10:38:17 -0300

    Adicionado efeito de diminuição de opacidade no menu quando o usuario rolar a pagina.

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const words = ["Qualidade na Didática", "Agilidade no Ensino", "Ensino Personalizado"];
-let count = 0;
-let index = 0;
-let currentWord = '';
-let letter = '';
+////--->> Navbar opacity
+window.addEventListener('scroll', () => {
+  if(window.scrollY > 180) {
+    document.querySelector('#navbar').style.opacity = 0.8;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+})
+
+
+////--->> Typing Effect
+  const words = ["Qualidade na Didática", "Agilidade no Ensino", "Ensino Personalizado"];
+  let count = 0;
+  let index = 0;
+  let currentWord = '';
+  let letter = '';
 
 (function type(){
+
   if(count === words.length){
     count = 0;
   }
